d0f68ffe13fe4be472b0627cf8709819c792d5b1
ung <ung6039@naver.com>
2021-04-28 20:57:18 +0900

    04-28

--- shop_java/src/main/java/com/myshop/app/HomeController.java
+++ shop_java/src/main/java/com/myshop/app/HomeController.java
@@ -33,100 +33,102 @@ import com.myshop.app.vo.MemberVO;
  */
 @Controller
 public class HomeController {
-	
-//	@Autowired
-//	UserService userService;
+
+	// @Autowired
+	// UserService userService;
 	@Autowired
 	MemberDAO memberdao;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value="/app", method = RequestMethod.GET)
+	@RequestMapping(value = "/app", method = RequestMethod.GET)
 	public String start() {
-		
+		System.out.println("sf");
 		System.out.println("start");
 		return "index";
 	}
+
 	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate );
-		
-		
+		model.addAttribute("serverTime", formattedDate);
+
 		return "WEB-INF/main/home";
 	}
-	@RequestMapping(value="/joinForm.do",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/joinForm.do", method = RequestMethod.GET)
 	public String join(HttpServletRequest request, Model model) {
 		return "WEB-INF/login/joinForm";
 	}
-	@RequestMapping(value="/JoinPro.do",method=RequestMethod.POST)
+
+	@RequestMapping(value = "/JoinPro.do", method = RequestMethod.POST)
 	public String join_p(HttpServletRequest request, Model model) {
 		boolean status = true;
-		if(status) {
+		if (status) {
 			String id = request.getParameter("id");
 			String pwd = request.getParameter("pwd");
 			String name = "웅";
 			String birthday = request.getParameter("birthday");
 			String birthdd = request.getParameter("birthdd");
 			String birthmm = request.getParameter("birthmm");
-			birthday = birthday+"-"+birthmm+"-"+birthdd;
+			birthday = birthday + "-" + birthmm + "-" + birthdd;
 			String addr = "서울시";
 			String img = "img/img.png";
 			Map map = new HashMap();
 			map.put("memberid", id);
-			map.put("pwd",pwd);
+			map.put("pwd", pwd);
 			map.put("birthday", birthday);
 			map.put("name", name);
-			map.put("info","123123");
+			map.put("info", "123123");
 			map.put("addr", addr);
-			map.put("img",img);
-			
+			map.put("img", img);
+
 			memberdao.save(map);
-			
+
 			return "redirect:/main.do";
-		}  
-		
+		}
+
 		else {
 			return "WEB-INF/login/login";
 		}
 	}
-	@RequestMapping(value="/about.do", method= RequestMethod.GET)
+
+	@RequestMapping(value = "/about.do", method = RequestMethod.GET)
 	public String about(Locale locale, Model model, HttpServletRequest request) {
-		logger.info("about",locale);
+		logger.info("about", locale);
 		String img = request.getParameter("img");
-		return "WEB-INF/about/detail?"+img;
+		return "WEB-INF/about/detail?" + img;
 	}
-	@RequestMapping(value="/login.do")
-	public String login(HttpSession session){
+
+	@RequestMapping(value = "/login.do")
+	public String login(HttpSession session) {
 		return "WEB-INF/login/login";
 	}
-	
-	@RequestMapping(value="login1.do", method=RequestMethod.POST)
-	public String login1(HttpServletRequest request, HttpSession session ) {
+
+	@RequestMapping(value = "login1.do", method = RequestMethod.POST)
+	public String login1(HttpServletRequest request, HttpSession session) {
 		String id = (String) request.getParameter("id");
-		String pwd= (String) request.getParameter("pwd");
+		String pwd = (String) request.getParameter("pwd");
 		Map map = new HashMap();
 		map.put("id", id);
 		map.put("pwd", pwd);
 		Optional<MemberVO> vo = memberdao.findId(map);
 		System.out.println(session.getAttribute("id"));
-		if(vo.isPresent()) {
-			logger.info("로그인 정보"+id+" "+ pwd);
+		if (vo.isPresent()) {
+			logger.info("로그인 정보" + id + " " + pwd);
 			session.setAttribute("id", id);
 			return "index";
-		}else {
+		} else {
 			return "WEB-INF/login/login?loginstate=0";
 		}
-		
-		
-	
+
 	}
-	
+
 }
