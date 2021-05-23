7ad58c20571195134c31ffcc215672ad1fdef192
ung <ung6039@naver.com>
2021-05-23 22:50:30 +0900

    2021-05-23

--- shop_java/src/main/java/com/myshop/app/service/TEST.java
+++ shop_java/src/main/java/com/myshop/app/service/TEST.java
@@ -6,4 +6,7 @@ public class TEST {
         System.out.println("asdfsadfsa");
 
     }
+    
+    public void test() {
+    }
 }
--- shop_java/src/main/java/com/myshop/main/MainController.java
+++ shop_java/src/main/java/com/myshop/main/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class MainController {
+	
 	@RequestMapping(value="/main.do", method = RequestMethod.GET)
 	public String main(HttpServletRequest request) {
 		request.setAttribute("main", "START");
